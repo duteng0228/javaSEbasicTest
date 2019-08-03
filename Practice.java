f765b21ab783e9353e6c6b583e1861b84b9554d0
dt <2731671905@qq.com>
2019-08-03 09:28:17 +0800

    提交

---
+++ Practice.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Practice{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		int getResult = getSum(num1,num2);
+		System.out.println(getResult);
+		System.out.println(getMistake(num1,num2));
+		System.out.println(getPlus(num1,num2));
+		System.out.println(getDivision(num1,num2));
+		
+	}	
+		
+	public static int getSum(int num1,int num2){
+		int result = num1+num2;
+		return result;	
+	}
+	public static int getMistake(int num1,int num2){
+		int result1 = num1-num2;
+		return result1;
+	}
+	public static int getPlus(int num1,int num2){
+		int result2 = num1*num2;
+		return result2;
+	}
+	public static int getDivision(int num1,int num2){
+		int result3 = num1/num2;
+		return result3;
+	}
+}
