512ffa198bbb4a66ef7dde430fa5d36fd9e529a4
dt <2731671905@qq.com>
2019-08-12 18:52:18 +0800

    练习

--- Practice.java
+++ Practice.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Practice{
+public class PracTice{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		int num1 = scanner.nextInt();
